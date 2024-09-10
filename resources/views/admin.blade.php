c6dc0d4b6db473023d002115a6e2f88fa510436f
axnish27 <itsaanish10@gmail.com>
2024-09-10 13:15:18 -0400

    Initial commit

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        @vite([ 'resources/css/app.css', 'resources/js/app.js'])
+
+    </head>
+    <body class="container ">
+        <h1 class="h1 text-center">Manage Employee Profiles</h1>
+        <button class=" btn btn-primary" style="margin-left: 11% ">New Employee</button>
+        <div class="cards d-flex flex-wrap justify-content-center  mx-auto gap-3">
+            <div class="card w-25 h-25 mt-3 " >
+                <div class="card-body d-flex   justify-content-evenly flex-column">
+                    <h4 class="card-title">
+                        Aanish Ramzan </h4>
+                    <h5 class="card-subtitle">Intern </h5>
+                    <div class="mt-2">2005-06-07</div>
+                    <div class="mt-2">aansh2710@gmail.com</div>
+                    <div class="mt-2">+94767762731</div>
+                    <div class="mt-2 mb-2">153/7 mullegma ambatenna</div>
+                    <button class="btn btn-primary mb-2"> Edit </button>
+                    <button class="btn btn-danger"> Delete </button>
+                </div>
+
+            </div>
+            <div class="card w-25 h-25 mt-3" >
+                <div class="card-body d-flex justify-content-evenly flex-column">
+                    <h4 class="card-title">
+                        Aanish Ramzan </h4>
+                    <h5 class="card-subtitle">
+                        Intern </h5>
+                    <div class="mt-2">2005-06-07</div>
+                    <div class="mt-2">aansh2710@gmail.com</div>
+                    <div class="mt-2">+94767762731</div>
+                    <div class="mt-2 mb-2">153/7 mullegma ambatenna</div>
+                    <button class="btn btn-primary mb-2"> Edit </button>
+                    <button class="btn btn-danger"> Delete </button>
+                </div>
+
+            </div>
+            <div class="card w-25 h-25 mt-3" >
+                <div class="card-body d-flex justify-content-evenly flex-column">
+                    <h4 class="card-title">
+                        Aanish Ramzan </h4>
+                    <h5 class="card-subtitle">
+                        Intern </h5>
+                    <div class="mt-2">2005-06-07</div>
+                    <div class="mt-2">aansh2710@gmail.com</div>
+                    <div class="mt-2">+94767762731</div>
+                    <div class="mt-2 mb-2">153/7 mullegma ambatenna</div>
+                    <button class="btn btn-primary mb-2"> Edit </button>
+                    <button class="btn btn-danger"> Delete </button>
+                </div>
+
+            </div>
+            <div class="card w-25 h-25 mt-3" >
+                <div class="card-body d-flex justify-content-evenly flex-column">
+                    <h4 class="card-title">
+                        Aanish Ramzan </h4>
+                    <h5 class="card-subtitle">
+                        Intern </h5>
+                    <div class="mt-2">2005-06-07</div>
+                    <div class="mt-2">aansh2710@gmail.com</div>
+                    <div class="mt-2">+94767762731</div>
+                    <div class="mt-2 mb-2">153/7 mullegma ambatenna</div>
+                    <button class="btn btn-primary mb-2"> Edit </button>
+                    <button class="btn btn-danger"> Delete </button>
+                </div>
+
+            </div>
+            <div class="card w-25 h-25 mt-3" >
+                <div class="card-body d-flex justify-content-evenly flex-column">
+                    <h4 class="card-title">
+                        Aanish Ramzan </h4>
+                    <h5 class="card-subtitle">
+                        Intern </h5>
+                    <div class="mt-2">2005-06-07</div>
+                    <div class="mt-2">aansh2710@gmail.com</div>
+                    <div class="mt-2">+94767762731</div>
+                    <div class="mt-2 mb-2">153/7 mullegma ambatenna</div>
+                    <button class="btn btn-primary mb-2"> Edit </button>
+                    <button class="btn btn-danger"> Delete </button>
+                </div>
+
+            </div>
+            <div class="card w-25 h-25 mt-3" >
+                <div class="card-body d-flex justify-content-evenly flex-column">
+                    <h4 class="card-title">
+                        Aanish Ramzan </h4>
+                    <h5 class="card-subtitle">
+                        Intern </h5>
+                    <div class="mt-2">2005-06-07</div>
+                    <div class="mt-2">aansh2710@gmail.com</div>
+                    <div class="mt-2">+94767762731</div>
+                    <div class="mt-2 mb-2">153/7 mullegma ambatenna</div>
+                    <button class="btn btn-primary mb-2"> Edit </button>
+                    <button class="btn btn-danger"> Delete </button>
+                </div>
+
+            </div>
+            <div class="card w-25 h-25 mt-3" >
+                <div class="card-body d-flex justify-content-evenly flex-column">
+                    <h4 class="card-title">
+                        Aanish Ramzan </h4>
+                    <h5 class="card-subtitle">
+                        Intern </h5>
+                    <div class="mt-2">2005-06-07</div>
+                    <div class="mt-2">aansh2710@gmail.com</div>
+                    <div class="mt-2">+94767762731</div>
+                    <div class="mt-2 mb-2">153/7 mullegma ambatenna</div>
+                    <button class="btn btn-primary mb-2"> Edit </button>
+                    <button class="btn btn-danger"> Delete </button>
+                </div>
+
+            </div>
+            <div class="card w-25 h-25 mt-3" >
+                <div class="card-body d-flex justify-content-evenly flex-column">
+                    <h4 class="card-title">
+                        Aanish Ramzan </h4>
+                    <h5 class="card-subtitle">
+                        Intern </h5>
+                    <div class="mt-2">2005-06-07</div>
+                    <div class="mt-2">aansh2710@gmail.com</div>
+                    <div class="mt-2">+94767762731</div>
+                    <div class="mt-2 mb-2">153/7 mullegma ambatenna</div>
+                    <button class="btn btn-primary mb-2"> Edit </button>
+                    <button class="btn btn-danger"> Delete </button>
+                </div>
+
+            </div>
+            <div class="card w-25 h-25 mt-3" >
+                <div class="card-body d-flex justify-content-evenly flex-column">
+                    <h4 class="card-title">
+                        Aanish Ramzan </h4>
+                    <h5 class="card-subtitle">
+                        Intern </h5>
+                    <div class="mt-2">2005-06-07</div>
+                    <div class="mt-2">aansh2710@gmail.com</div>
+                    <div class="mt-2">+94767762731</div>
+                    <div class="mt-2 mb-2">153/7 mullegma ambatenna</div>
+                    <button class="btn btn-primary mb-2"> Edit </button>
+                    <button class="btn btn-danger"> Delete </button>
+                </div>
+
+            </div>
+
+        </div>
+
+    </body>
+</html>
+
