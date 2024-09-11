ac7a80a0bac30e19082552d4d2779efccf611cef
axnish27 <itsaanish10@gmail.com>
2024-09-11 15:46:35 -0400

    configurations removed

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,11 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 window.axios = axios;
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
-import $ from 'jquery';
+import $ from "jquery";
 window.$ = $;
 
-import DataTable from 'datatables.net-dt';
-
-
-
+import DataTable from "datatables.net-dt";
