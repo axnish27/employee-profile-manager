6b022c9e1e565047cbdf7b4dcc3d7d6bc7ea2921
axnish27 <itsaanish10@gmail.com>
2024-09-12 15:49:45 -0400

    added icons for better UI

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -29,7 +29,14 @@
 
         <main class="container" >
             <h1 class="h1 text-center mt-5" >Manage Employee Profiles</h1>
-            <button class=" btn btn-primary mt-3"  data-bs-toggle="modal" data-bs-target="#modal-employee-create">New Employee</button>
+
+            <button class="btn btn-primary rounded-circle p-0 mt-2 mb-2 "  data-bs-toggle="modal" data-bs-target="#modal-employee-create"  >
+                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+                    <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+                  </svg>
+
+            </button>
+
             <div class="modal fade" id="modal-employee-create" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
                 <div class="modal-dialog modal-dialog-centered">
                   <div class="modal-content">
@@ -63,6 +70,7 @@
                 $('#myTable').DataTable({
                     serverSide: true,
                     processing: true,
+                    fixedHeader: true,
                     ajax: {
                         url: 'employee',
                         dataSrc: ''
@@ -76,12 +84,25 @@
                         { data: 'address' , title:'Address'},
                         { data: null ,
                             render: function (data, type, row) {
-                                return `<button id="btn-edit-${row.id}" class="btn btn-primary" data-id="${row.id}" data-bs-toggle="modal" data-bs-target="#modal-task-create">Edit</button>
-                                        <button id="btn-dlt-${row.id}" class="btn btn-danger" data-id="${row.id}" >Delete</button>`;
+                                return `  <button class="d-inline btn  edit p-0 " id="btn-edit-${row.id}" data-id="${row.id}" >
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="dodgerblue" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                                                <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+                                            </svg>
+                                          </button>
+                                           <button  id="btn-dlt-${row.id}" class="btn p-0 d-inline" data-id="${row.id}" >
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="red" class="bi bi-trash" viewBox="0 0 16 16">
+                                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                                                <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                                            </svg>
+                                        </button> `
                             }
                         },
                     ]
                 });
+
+
+
 
 
 
