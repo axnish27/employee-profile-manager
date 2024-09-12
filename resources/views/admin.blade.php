ea694cc66d9d65e94ace83c4e9c68f88648db4f8
axnish27 <itsaanish10@gmail.com>
2024-09-12 17:57:13 -0400

    reload after dlt fixed

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -102,14 +102,15 @@
                 $('#myTable tbody').on('click', 'button[id^="btn-dlt"]', function (e) {
                     let id = $(this).data('id');
                     deleteEmployee(id)
+                    let $button = $(this)
+                    table.row($button.parents('tr') ).remove().draw();
+
                 });
 
                 function deleteEmployee(id){
                     axios.delete(`employee/${id}`)
                     .then(function (response){
-                        if (response.status == 200){
-                            window.location.reload();
-                        }
+                        console.log("Delted");
                     });
                 }
             });
