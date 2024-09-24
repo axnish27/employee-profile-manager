0b17ae638f141e2bee149187f5889f2dbe371124
axnish27 <itsaanish10@gmail.com>
2024-09-24 14:29:39 -0400

    Set Up Alert for company

--- resources/views/company/index.blade.php
+++ resources/views/company/index.blade.php
@@ -8,7 +8,7 @@
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    
+
     <body class="container">
         <h1 class="h1 text-center">Company Manager </h1>
         <a href="{{ route('companys.create')}}" class="btn btn-primary"> Create New Company</a>
@@ -20,13 +20,31 @@
                     </a>
                     <br>
                     <a href="{{ route('companys.edit' , $company->id) }}" class="btn btn-success  d-inline"> Edit</a>
-                    <form action="{{ route('companys.destroy' , $company->id ) }}"
-                        method="POST"
-                        class="d-inline">
-                        @method('DELETE')
-                        @csrf
-                        <button class="btn btn-danger">Delete</button>
-                    </form>
+
+                    <div class="modal fade" id="deletConfirmation" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="deletConfirmationLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                          <div class="modal-content">
+                            <div class="modal-header">
+                              <h1 class="modal-title fs-5" id="deletConfirmationLabel">Are You Sure to Delete the Company Details?? </h1>
+                              <button type="button" class="btn-close btn-close-dlt" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                              This Action wil Delete Company and the records related to him Such as Projects
+                            </div>
+                            <div class="modal-footer">
+                              <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button>
+                              <form action="{{ route('companys.destroy' , $company->id ) }}"
+                                method="POST"
+                                class="d-inline">
+                                @method('DELETE')
+                                @csrf
+                                <button type="button" id="btn-dlt"  class="btn btn-danger btn-dlt">Delete</button>
+                             </form>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    <button class="btn btn-danger btn-dlt-modal p-1 d-inline" data-bs-toggle="modal" data-bs-target="#deletConfirmation" >Delete</button>
                 </li>
              @endforeach
         </ul>
