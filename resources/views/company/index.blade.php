46d410416a5f92dbe3329598e6fbe779ae1d73c3
Aanish Ramzan <110470875+aanish27@users.noreply.github.com>
2024-10-08 13:47:33 -0400

    layout changes for Company

--- resources/views/company/index.blade.php
+++ resources/views/company/index.blade.php
@@ -92,70 +92,99 @@
 
       <script type="module">
         $(function () {
-
           let table= $('#myTable').DataTable({
-            layout: {
-                        topStart: null,
-                        topEnd: null,
-                        top1Start:{
-                            pageLength: {
-                            placeholder: 'Filter'
-                            },
-                            search: {
-                                placeholder: 'Type search here'
-                            },
-                        },
-                        top1End:{
-                            buttons: [{
-                                text: '<i class="bi bi-plus-lg"></i> ',
-                                attr: {
-                                    id: 'btn-add-record',
-                                    'data-bs-toggle': 'modal',
-                                    'data-bs-target': '.modal'
-                                },
-                                action: function (e, dt, node, config, cb) {
-                                    storeCompany()
-                                }
-                            }]
-                        }
+                fixedColumns: true,
+                scrollCollapse: true,
+                scrollY: 500,
+                scrollX: true,
+                responsive: true,
+                layout: {
+                  topStart: null,
+                  topEnd: null,
+                  top1Start:{
+                      pageLength: {
+                      placeholder: 'Filter'
+                      },
+                      search: {
+                          placeholder: 'Type search here'
+                      },
+                  },
+                  top1End:{
+                      buttons: [{
+                          text: '<i class="bi bi-plus-lg"></i> New',
+                          attr: {
+                              id: 'btn-add-record',
+                              'data-bs-toggle': 'modal',
+                              'data-bs-target': '.modal'
+                          },
+                          action: function (e, dt, node, config, cb) {
+                              storeCompany()
+                          }
+                      }]
+                  }
+              },
+              language: {
+                  lengthMenu:  ' _MENU_',
+                  search: ' '
+              },
+              serverSide: true,
+              processing: true,
+              ajax: {
+                  url: 'company/draw',
+              },
+              columns: [
+                {
+                  data: null ,
+                  title: 'Actions',
+                  render: function (data, type, row) {
+                      return `  <button class="d-inline btn  btn-edit p-0 " id="btn-edit-${row.id}" data-id="${row.id}" data-bs-toggle="modal" data-bs-target=".modal" >
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="dodgerblue" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                      <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                                      <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+                                  </svg>
+                                </button>
+                                  <button class="btn btn-dlt-modal p-0 d-inline" data-id="${row.id}" data-bs-toggle="modal" data-bs-target="#deletConfirmation" >
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="red" class="bi bi-trash" viewBox="0 0 16 16">
+                                      <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                                      <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                                  </svg>
+                              </button> `
                     },
-                    language: {
-                        lengthMenu:  ' _MENU_',
-                        search: ' '
-                    },
-
-
-
-                serverSide: true,
-                processing: true,
-                ajax: {
-                    url: 'company/draw',
+                    width: '6%',
                 },
-                columns: [
-                  { data: null , title: 'Actions',
-                        render: function (data, type, row) {
-                            return `  <button class="d-inline btn  btn-edit p-0 " id="btn-edit-${row.id}" data-id="${row.id}" data-bs-toggle="modal" data-bs-target=".modal" >
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="dodgerblue" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                                            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
-                                        </svg>
-                                      </button>
-                                        <button class="btn btn-dlt-modal p-0 d-inline" data-id="${row.id}" data-bs-toggle="modal" data-bs-target="#deletConfirmation" >
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="red" class="bi bi-trash" viewBox="0 0 16 16">
-                                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
-                                            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
-                                        </svg>
-                                    </button> `
-                        }
-                    },
-                    { data: 'name', title:'Company' },
-                    { data: 'branch', title:'Branch'},
-                    { data: 'country' , title: 'Country' ,},
-                    { data: 'address' , title:'Address' },
-                    { data: 'employees_count' , title:'Employees' },
-                    { data: 'projects_count' , title:'Projects' },
-                ],
-            });
+                {
+                  data: 'name',
+                  title:'Company',
+                  render: DataTable.render.ellipsis( 50 )
+                },
+                {
+                  data: 'branch',
+                  title:'Branch',
+                  render: DataTable.render.ellipsis( 26 )
+                },
+                {
+                  data: 'country' ,
+                  title: 'Country' ,
+                  render: DataTable.render.ellipsis( 26 )
+                },
+                {
+                  data: 'address' ,
+                  title:'Address',
+                  render: DataTable.render.ellipsis( 100 )
+                },
+                {
+                  data: 'employees_count' ,
+                  title:'Employees',
+                  className: 'dt-center'
+                },
+                {
+                  data: 'projects_count',
+                  title:'Projects',
+                  className: 'dt-center'
+                },
+              ],
+
+             });
 
             // Delete Company
             $('#myTable tbody').on('click' , '.btn-dlt-modal' ,function (e) {
@@ -236,7 +265,6 @@
                 .catch(function (response){
                     displayToast(response , "error")
                 });
-
             });
 
             //Toast Alerts
